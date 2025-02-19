8b487e72414baf7d966afa27408a0dd8f8185acd
laiquanghai2k6 <yortvrluer@gmail.com>
2025-02-19 21:17:59 +0700

    background and objectTexture

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,78 @@
 #include <bits/stdc++.h>
 #include <SDL2/SDL.h>
-#include <vector>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_Image.h>
 
+#include <vector>
 using namespace  std;
+int const SCREEN_WIDTH = 1280;
+int const SCREEN_HEIGHT = 720;
 int main(int argc, char* argv[]){
-    if(SDL_Init(SDL_INIT_VIDEO) > 0){
-        cout << "sdl fail video" << SDL_GetError()<<endl;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    bool running = true;
+    SDL_Window* window;
+    SDL_Rect source{50,SCREEN_HEIGHT-250,70,70};
+    // SDL_Rect dest{10,10,Window_H-20,Window_W-20};
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        cout << "Lỗi khởi tạo SDL2_image: " << IMG_GetError() << endl;
+        system("pause");
+        return -1;
     }
-    if(!(IMG_Init(IMG_INIT_PNG))){
-        cout << "sdl fail image" << SDL_GetError()<<endl;
+    window = SDL_CreateWindow("snakeio",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,SCREEN_WIDTH ,SCREEN_HEIGHT,0);
+    SDL_Renderer* renderer;
+    renderer = SDL_CreateRenderer(window,-1,0);
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+    cout << "Lỗi SDL_image: " << IMG_GetError() << endl;
+    return -1;
+}
+// SDL_Rect rect = {50, 50, 100, 100};  // Vị trí và kích thước của hình chữ nhật
+
+
+// cout << 5 << endl;
+    SDL_Texture* background = IMG_LoadTexture(renderer, "C:/Users/ADMIN/Desktop/games/src/bg.png");
+    SDL_Texture* objectTexture = IMG_LoadTexture(renderer, "C:/Users/ADMIN/Desktop/games/src/gameObject.png");
+
+
+    
+    SDL_Event event;
+    // Vòng lặp game
+    float speed;
+    
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false; // Thoát game khi nhấn "X"
+                return 0;
+            }
+            if(event.type == SDL_KEYDOWN){
+                switch(event.key.keysym.sym){
+                    case SDLK_UP:
+                    source.y-=5;
+                    break;
+                    case SDLK_DOWN:
+                    source.y+=5;
+                    break;
+                    case SDLK_LEFT:
+                    source.x-=5;
+                    break;
+                    case SDLK_RIGHT:
+                    source.x+=5;
+                    break;
+                }
+            }
+        }
+        
+        SDL_RenderClear(renderer);
+    
+        // Vẽ ảnh nền full màn hình
+        SDL_Rect bgRect = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
+      
+        SDL_RenderCopy(renderer, background, NULL, &bgRect);
+        SDL_RenderCopy(renderer, objectTexture, NULL, &source);
+
+        // Vẽ các đối tượng khác ở trên nền
+        SDL_RenderPresent(renderer);
+       
+        SDL_Delay(16);
     }
-    if(!(SDL_Init(SDL_INIT_AUDIO)) < 0){
-        return 1;
-        cout << "sdl fail audio" << SDL_GetError()<<endl;
-    }
-    if(TTF_Init() == -1){
-        cout << "ttf error"<<endl;
-        return 1;
-    }
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("test",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_SHOWN);
-    SDL_Renderer *render = SDL_CreateRenderer(window,-1,0);
-    SDL_Delay(10000);
-   
+    return 0;
 }
